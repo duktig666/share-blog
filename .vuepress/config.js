773f52ba52c639cfbc3ef9598f671fe6c4e97768
duktig666(RenShiWei) <1487660836@qq.com>
2022-01-22 14:12:08 +0800

    优化 .gitignore

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -118,6 +118,12 @@ module.exports = {
                 "link": "https://elltor.com/"
             },
             {
+                "title": "午后南杂",
+                "desc": "Enjoy when you can, and endure when you must.",
+                "email": "1156743527@qq.com",
+                "link": "https://www.recoluan.com"
+            },
+            {
                 "title": "vuepress-theme-reco",
                 "desc": "A simple and beautiful vuepress Blog & Doc theme.",
                 "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
