7268cd7bcf08bc4488fd8f4dccfad780292d8c90
RenShiWei <ren_shi_wei@qq.com>
2023-03-21 20:20:42 +0800

    workflows update

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push git@github.com:duktig666/share-blog.git main:gh-pages
+git push git@github.com:duktig666/share-blog.git main:gh-pages -f
 
 cd -
